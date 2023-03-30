6aa7c1b8e9d3b8a90e223c222083502d899eb258
Masud <ranamasud0701@gmail.com>
2023-03-30 21:18:37 +0600

    add card

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+    const [products,setProducts] = useState([]);
+    useEffect(()=>{
+         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+         .then(res=>res.json())
+         .then(data=>setProducts(data))
+    },[])
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+               
+                {
+                    products.map(product=>
+                        <Product  
+                          key={product.id}
+                          product={product}
+                         
+                        ></Product> )
+                }
+            </div>
+            <div className='card-container'>
+                 <h2>order summary</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
