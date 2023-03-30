cf273ee562ac95eb011b16938f68f1b3a2984437
Masud <ranamasud0701@gmail.com>
2023-03-30 22:45:45 +0600

    addad item

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css';
 const Product = (props) => {
 
- const { img,name,seller,price,ratings }= props.product;
-  
+    const { img, name, seller, price, ratings } = props.product;
+    const handler = props.handler;
+
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
-           <div className='product-info'>
-           <h6 className='product-name'>{name}</h6>
-            <p>Price: ${price}</p>
-            <p>Manufacturing :{seller}</p>
-            <p>Ratings:{ratings}</p>
-           </div>
-           <button className='btn'>Add card</button>
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturing :{seller}</p>
+                <p>Ratings:{ratings}</p>
+            </div>
+            <button onClick={() => handler(props.product)} className='btn'>Add card
+            <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,19 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products,setProducts] = useState([]);
+
+    const [card,setCard]= useState([]);
     useEffect(()=>{
          fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
          .then(res=>res.json())
          .then(data=>setProducts(data))
     },[])
+
+    const handler=(product)=>{
+        const newCard = [...card,product];
+        setCard(newCard);
+
+    }
     return (
         <div className='shop-container'>
             <div className='product-container'>
@@ -18,12 +26,14 @@ const Shop = () => {
                         <Product  
                           key={product.id}
                           product={product}
+                          handler={handler}
                          
                         ></Product> )
                 }
             </div>
             <div className='card-container'>
                  <h2>order summary</h2>
+                 <p>Selected items : {card.length}</p>
             </div>
         </div>
     );
